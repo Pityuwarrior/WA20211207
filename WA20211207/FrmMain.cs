4686885f110ab9d0af640c55d6b28b1012d8a46f
nove.norbert <nove.norbert@28-05>
2021-12-07 09:40:39 +0100

    Add project files.

---
+++ WA20211207/FrmMain.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WA20211207
+{
+    public partial class FrmMain : Form
+    {
+        public Button[,] Palya { get; set; }
+        public int Meret { get; set; }
+        public FrmMain()
+        {
+            InitializeComponent();
+            Palya = new Button[10, 10];
+            Meret = 50;
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            for (int oszlop  = 0; oszlop < Palya.GetLength(0); oszlop++)
+            {
+                for (int sor = 0; sor < Palya.GetLength(1); sor++)
+                {
+                    Palya[oszlop, sor] = new Button()
+                    {
+                        Bounds = new Rectangle()
+                        {
+                            X = oszlop * Meret,
+                            Y = sor * Meret,
+                            Height = Meret,
+                            Width = Meret,
+                        },
+                        Text = $"s: {sor}; o:{oszlop}",
+                    };
+                    Palya[oszlop, sor].MouseDown += FrmMain_MouseDown;
+                    Controls.Add(Palya[oszlop, sor]);
+                        
+                }
+            }
+        }
+
+        private void FrmMain_MouseDown(object sender, MouseEventArgs e)
+        {           
+                (sender as Button).BackColor =
+                    e.Button == MouseButtons.Left ?
+                    Color.Green : Color.Red;
+            
+            
+        }
+    }
+}
---
+++ WA20211207/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("WA20211207")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WA20211207")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3e7fc3d7-e091-46bc-9989-f73cceee6e7d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
